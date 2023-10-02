63e88acbcbdcf23752153917db050726ad3c8c95
Gourav Mohanty <gouravmohanty2001@gmail.com>
2023-10-02 10:23:39 +0530

    feat: Loader added

---
+++ atlan-sql/src/components/load/load.js
@@ -0,0 +1,14 @@
+import "./Load.css";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loader__stick loader__stick--1"></div>
+      <div className="loader__stick loader__stick--2"></div>
+      <div className="loader__stick loader__stick--3"></div>
+      <div className="loader__stick loader__stick--4"></div>
+    </div>
+  );
+};
+
+export default Loader;
